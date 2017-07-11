e811c9ff1311587188430018678c037274cd2216
Jonathan JEAN <jonathan-jean@users.noreply.github.com>
2017-07-11 11:14:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -121,6 +121,7 @@ class SettingsList extends React.Component {
               key={item.id}
               style={item.editableTextStyle ? item.editableTextStyle : styles.editableText}
               placeholder = {item.placeholder}
+              secureTextEntry = {item.secureTextEntry}
               onSubmitEditing={(text) => item.onSubmitEditing(text)}
               onChangeText={(text) => item.onTextChange(text)}
               value={item.value} />
